0501ec28450f40ec0533eb6654ccd45971208c85
Mohammad_emami <1385.mohammademami@gmail.com>
2026-01-28 12:56:33 +0330

    arain koni

--- inventory_mvp/settings.py
+++ inventory_mvp/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mvp-dev-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
